5901291e39f8e0f8d24e670309d277568e264a5f
Alexander Snov <snov.contact@gmail.com>
2019-07-04 01:36:50 +0700

    (data): Unidirection graph fixes
    * depthFirstSearch: fill `isMarked` array with `false` by default
    * fix tests

--- src/structures/graph.test.ts
+++ src/structures/graph.test.ts
@@ -1,11 +1,13 @@
 import { UnidirectionalGraph, depthFirstSearch, VertexConnection } from './graph';
 
-const edges = [[0, 1], [1, 2], [2, 0]] as VertexConnection[];
-const verticesCount = edges.length;
+const edgesConnected = [[0, 1], [1, 2], [2, 0]] as VertexConnection[];
+const edgesUnconnected = [[0, 1]] as VertexConnection[];
+
+const verticesCount = 3;
 
 describe(`Unidirectional Graph`, () => {
   it(`Created from initial data`, () => {
-    const graph = new UnidirectionalGraph(verticesCount, edges);
+    const graph = new UnidirectionalGraph(verticesCount, edgesConnected);
 
     expect(graph.adjacentTo(0)).toStrictEqual({
       1: 1,
@@ -24,7 +26,7 @@ describe(`Unidirectional Graph`, () => {
   });
 
   it(`Depth First Search detects connected graph`, () => {
-    const graph = new UnidirectionalGraph(verticesCount, edges);
+    const graph = new UnidirectionalGraph(verticesCount, edgesConnected);
     const search = depthFirstSearch(graph, 0);
     for (let i = 0; i < graph.verticesCount; i++) {
       expect(search.marked(i)).toBe(true);
@@ -33,12 +35,11 @@ describe(`Unidirectional Graph`, () => {
   });
 
   it(`Depth First Search detects unconnected graph`, () => {
-    const graph = new UnidirectionalGraph(verticesCount, edges);
+    const graph = new UnidirectionalGraph(verticesCount, edgesUnconnected);
     const search = depthFirstSearch(graph, 0);
-    for (let i = 0; i < graph.verticesCount; i++) {
-      expect(search.marked(i)).toBe(true);
-    }
-    expect(search.count).toBe(graph.verticesCount);
+    expect(search.marked(0)).toBe(true);
+    expect(search.marked(1)).toBe(true);
+    expect(search.marked(2)).toBe(false);
+    expect(search.count).toBe(graph.verticesCount - 1);
   });
-
 });
